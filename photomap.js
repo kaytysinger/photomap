cea11066444e90d6d5ef8ab39280df4b19ae802a
kaytysinger <98780298+kaytysinger@users.noreply.github.com>
2022-04-25 17:09:54 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -20,8 +20,9 @@ function createPhotoMap () {
   var map = L.map('map').setView([33.75236, -84.37761], 13;
   
   // create basemap object. See examples at https://leaflet-extras.github.io/leaflet-providers/preview/
-  var basemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+  var basemap = var basemap = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
+ maxZoom: 16,
+ attribution: 'USGS'
 }).addTo(map);
   
   // use Papa Parse (papaparse.com) to get the Google Sheets CSV
